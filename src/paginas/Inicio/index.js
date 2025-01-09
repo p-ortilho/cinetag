d2d00eb178a9fa45c4461cc3d170e8d674376f1a
p-ortilho <cainportilho@gmail.com>
2025-01-09 20:37:28 -0300

    adição do card ao componente de página de inicio

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Card from "componentes/Card";
 import Rodape from "componentes/Rodape";
 import Titulo from "componentes/Titulo";
 
@@ -13,6 +14,7 @@ const Inicio = () => {
                     Um lugar para guardar seus vídeos e filmes!
                 </h1>
             </Titulo>
+            <Card titulo={'Billie'} id={'1'} imagem={'https://media.globalcitizen.org/thumbnails/bc/df/bcdf3412-5ec9-4ddd-ba4d-d409623b225c/billieeilish.jpg__1600x900_q85_crop_subsampling-2.jpg'}/>
             <Rodape/>
         </>
     );
