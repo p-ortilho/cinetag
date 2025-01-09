aa89d73634b546f3c7ec691b0757dbe36fbadf07
p-ortilho <cainportilho@gmail.com>
2025-01-09 19:35:45 -0300

    criação do rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,13 @@
+import styles from './Rodape.module.css';
+
+const Rodape = () => {
+    return(
+        <footer className={styles.rodape}>
+            <p>
+                Desenvolvido por <strong>Cainã Portilho</strong>
+            </p>
+        </footer>
+    );
+};
+
+export default Rodape;
