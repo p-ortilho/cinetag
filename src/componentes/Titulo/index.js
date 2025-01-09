430f55b2d4cec2ac59c5e1479b3536d083c6335e
p-ortilho <cainportilho@gmail.com>
2025-01-09 20:06:07 -0300

    criação do titulo

---
+++ src/componentes/Titulo/index.js
@@ -0,0 +1,11 @@
+import styles from './Titulo.module.css'
+
+const Titulo = ({children}) => {
+    return(
+        <div className={styles.titulo}>
+            {children}
+        </div>
+    );
+};
+
+export default Titulo;
