3e6f7c0197806d9c83f7c7196c3eee8400847189
p-ortilho <cainportilho@gmail.com>
2025-01-09 20:06:20 -0300

    criação do banner

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,10 @@
+import styles from './Banner.module.css';
+
+const Banner = ({imagem}) => {
+    return(
+        <div className={styles.banner} style={{backgroundImage: `url(/imagens/banner-${imagem}.png)`}}>
+        </div>
+    );
+};
+
+export default Banner;
