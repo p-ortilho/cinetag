d2b586a689cdf8e8d3de58cc328e6c548acafde0
p-ortilho <cainportilho@gmail.com>
2025-01-09 20:06:42 -0300

    adição do banner e do titulo a página principal

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,10 +1,18 @@
+import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
 import Rodape from "componentes/Rodape";
+import Titulo from "componentes/Titulo";
 
 const Inicio = () => {
     return(
         <>
             <Cabecalho/>
+            <Banner imagem={'home'}/>
+            <Titulo>
+                <h1>
+                    Um lugar para guardar seus vídeos e filmes!
+                </h1>
+            </Titulo>
             <Rodape/>
         </>
     );
