c3aad52ba0370d5bb63e1000e03e5959285d4bc4
p-ortilho <cainportilho@gmail.com>
2025-01-09 23:36:15 -0300

    modificando a imagem do banner de maneira dinamica

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,6 +1,11 @@
 import styles from './Banner.module.css';
+import { useLocation } from 'react-router';
 
-const Banner = ({imagem}) => {
+const Banner = () => {
+
+    const rota = useLocation();
+    const imagem = rota.pathname === '/'? 'home' : 'favoritos';
+
     return(
         <div className={styles.banner} style={{backgroundImage: `url(/imagens/banner-${imagem}.png)`}}>
         </div>
