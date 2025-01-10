7e3c1646319df8e0bf524d7397a56aa8915b8316
p-ortilho <cainportilho@gmail.com>
2025-01-09 23:40:58 -0300

    modiicação de ajustes

---
+++ src/paginas/Favoritos/index.js
@@ -0,0 +1,11 @@
+import styles from './Favoritos.module.css';
+
+const Favoritos = () => {
+    return(
+        <div className={styles.favoritos}>
+            alguma coisa
+        </div>
+    );
+};
+
+export default Favoritos;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,28 @@
+import Favoritos from "paginas/Favoritos";
 import Inicio from "./paginas/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+import Container from "componentes/Container";
+import Banner from "componentes/Banner";
+import Titulo from "componentes/Titulo";
+import FavoritosProvider from "context/FavoritosContextos";
 
 const AppRoutes = () => {
     return(
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio/>}/>
-            </Routes>
+            <Cabecalho/>
+            <Banner/>
+            <Titulo/>
+            <Container>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio/>}/>
+                        <Route path="/favoritos" element={<Favoritos/>}/>
+                    </Routes>
+                </FavoritosProvider>
+            </Container>
+            <Rodape/>
         </BrowserRouter>
     );
 };
