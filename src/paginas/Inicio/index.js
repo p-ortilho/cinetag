e5f43dd19aadef5b636e920cee45d0732dfc7506
p-ortilho <cainportilho@gmail.com>
2025-01-09 23:38:46 -0300

    ajustes na pagina de inicio para tornar as rotas dinamicas

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,29 +1,14 @@
-import Banner from "componentes/Banner";
-import Cabecalho from "componentes/Cabecalho";
 import Card from "componentes/Card";
-import Rodape from "componentes/Rodape";
-import Titulo from "componentes/Titulo";
 import styles from './Inicio.module.css';
 import videos from 'json/db.json'
 
 const Inicio = () => {
     return(
-        <>
-            <Cabecalho/>
-            <Banner imagem={'home'}/>
-            <Titulo>
-                <h1>
-                    Um lugar para guardar seus vídeos e filmes!
-                </h1>
-            </Titulo>
-            <section className={styles.container}>
-                {videos.map((video) => {
-                    return <Card {...video} key={video.id}/>
-                })}
-            </section>
-            
-            <Rodape/>
-        </>
+        <section className={styles.container}>
+            {videos.map((video) => {
+                return <Card {...video} key={video.id}/>
+            })}
+        </section>
     );
 };
 
