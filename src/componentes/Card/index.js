5e086d0d8c4e1b97a169eb6bb07557928a66428b
p-ortilho <cainportilho@gmail.com>
2025-01-10 23:32:45 -0300

    configurando a funcionalidade de adicionar favoritos

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,12 +1,18 @@
+import { useFavoritos } from 'hooks/useFavoritos';
 import styles from './Card.module.css';
-import iconeFavoritar from './desfavoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
+import iconeFavoritar from './favoritar.png';
 
 const Card = ({titulo, id, imagem}) => {
+    const {favoritos, adicionarFavorito} = useFavoritos();
+    // verifica se o item já foi favoritado
+    const favoritoAtivo = !(favoritos.some(item => item.id === id));
+    const icone = favoritoAtivo ?   iconeDesfavoritar : iconeFavoritar;
     return(
         <div className={styles.container}>
             <img src={imagem} alt={titulo}/>
             <h1>{titulo}</h1>
-            <img src={iconeFavoritar} alt="Favoritar" className={styles.favoritar}/>
+            <img src={icone} alt="Favoritar" className={styles.favoritar} onClick={() => {adicionarFavorito({id, titulo, imagem})}}/>
         </div>
     );
 };
